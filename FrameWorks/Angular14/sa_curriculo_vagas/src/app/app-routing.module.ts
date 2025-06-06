68ab35d0bda246046f770ebbb962fb8a0e9ee681
GuilhermeXavier08 <guilhermecxavier123@gmail.com>
2025-06-06 11:23:48 -0300

    06.06.2025

---
+++ FrameWorks/Angular14/sa_curriculo_vagas/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './view/inicio/inicio.component';
+import { CurriculosComponent } from './view/curriculos/curriculos.component';
+import { VagasComponent } from './view/vagas/vagas.component';
+import { PainelVagasComponent } from './view/painel-vagas/painel-vagas.component';
+import { PainelCurriculosComponent } from './view/painel-curriculos/painel-curriculos.component';
+
+const routes: Routes = [
+  {path: "", component: InicioComponent},
+  {path: "curriculos", component: CurriculosComponent},
+  {path: "vagas", component: VagasComponent},
+  {path: "painel-vagas", component: PainelVagasComponent},
+  {path: "painel-curriculos", component: PainelCurriculosComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
